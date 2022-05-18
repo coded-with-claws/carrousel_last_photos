010223e6b0e1eeeae5541531953fd256bf9326b7
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2022-05-18 22:05:54 +0200

    functions

--- start_diapo.sh
+++ start_diapo.sh
@@ -4,63 +4,66 @@
 #@reboot    /home/pi/diapo/start_diapo.sh
 #* * * * *  /home/pi/diapo/start_diapo.sh
 
-source /home/pi/diapo/diapo.conf
+function main() {
 
-if [ -f $TOOL_DIR/diapo_mutex ]
-then
-  exit
-fi
+  source /home/pi/diapo/diapo.conf
 
-echo "[*] $0 demarre" &>$LOG_FILE
-touch $TOOL_DIR/diapo_mutex
+  if [ -f $TOOL_DIR/diapo_mutex ]
+  then
+    exit
+  fi
 
-DIR_SYMLINK=/tmp/symlinkdiapo
-rm -fr $DIR_SYMLINK
-mkdir $DIR_SYMLINK
+  echo "[*] $0 demarre" &>$LOG_FILE
+  touch $TOOL_DIR/diapo_mutex
 
-# Symlink vers les deux premieres photos (qui sont mises en cache par fbi donc non modifiables)
-echo "[*] init - linker les deux premieres photos fixes $PHOTO1 et $PHOTO2 dans le repertoire DIR_SYMLINK" &>>$LOG_FILE
-ln -sf $PHOTO_DIR/$PHOTO1 ${DIR_SYMLINK}/link_01
-ln -sf $PHOTO_DIR/$PHOTO2 ${DIR_SYMLINK}/link_02
+  DIR_SYMLINK=/tmp/symlinkdiapo
+  rm -fr $DIR_SYMLINK
+  mkdir $DIR_SYMLINK
 
-# Preparer les liens symboliques pour les 30 dernieres photos (30 x 10s = 5min)
-echo "[*] init - linker les photos dans le repertoire $DIR_SYMLINK" &>>$LOG_FILE
-symlink_i=3
-find $PHOTO_DIR -type f -printf "%C@ %p\n" | sort -n | cut -f2- -d" " | tail -$NB_PHOTO_DIAPO | tr '\n' '\0' | 
-  while IFS= read -r -d '' file; do 
-      i=`printf "%02d" $symlink_i`
-      #echo "link_$i" 
-      ln -sf "$file" ${DIR_SYMLINK}/"link_$i"
-      symlink_i=$(($symlink_i + 1))
+  # Symlink vers les deux premieres photos (qui sont mises en cache par fbi donc non modifiables)
+  echo "[*] init - linker les deux premieres photos fixes $PHOTO1 et $PHOTO2 dans le repertoire DIR_SYMLINK" &>>$LOG_FILE
+  ln -sf $PHOTO_DIR/$PHOTO1 ${DIR_SYMLINK}/link_001
+  ln -sf $PHOTO_DIR/$PHOTO2 ${DIR_SYMLINK}/link_002
+
+  # Preparer les liens symboliques pour les 30 dernieres photos (30 x 10s = 5min)
+  echo "[*] init - linker les photos dans le repertoire $DIR_SYMLINK" &>>$LOG_FILE
+  make_links
+
+  # lancer fbi (automatiquement en background)
+  sudo fbi -noverbose -T 1 -a -t $DUREE_PHOTO -cachemem 0 -blend 500 -nocomments $DIR_SYMLINK/* >/dev/null 2>&1
+  PID_FBI=$(pgrep fbi)
+  echo "[+] fbi demarre (PID $PID_FBI) sur le repertoire $DIR_SYMLINK" &>>$LOG_FILE
+  clear
+
+  # Boucle principale
+  while [ ! -f $TOOL_DIR/STOP ]; do 
+    
+    # attendre jusqu'à ce qu'il ne reste que 30 sec avant la fin du diaporama
+    # puis récupérer les 30 dernières photos (30 x 10s = 5min) (qui seront affichése a la prochaine itération)
+    NB_FIC=`find $DIR_SYMLINK -type l |wc -l`
+    DUREE_TEMPO=$(($NB_FIC * $DUREE_PHOTO - 30))
+    echo "[*] attendre $DUREE_TEMPO sec" &>>$LOG_FILE
+    sleep $DUREE_TEMPO &>/dev/null
+    echo "[+] fin attente" &>>$LOG_FILE
+    make_links
+    echo "[+] photos linkees dans le repertoire $DIR_SYMLINK" &>>$LOG_FILE
+
   done
 
-# lancer fbi (automatiquement en background)
-sudo fbi -noverbose -T 1 -a -t $DUREE_PHOTO -cachemem 0 -blend 500 -nocomments $DIR_SYMLINK/* >/dev/null 2>&1
-PID_FBI=$(pgrep fbi)
-echo "[+] fbi demarre (PID $PID_FBI) sur le repertoire $DIR_SYMLINK" &>>$LOG_FILE
-clear
+  rm $TOOL_DIR/diapo_mutex
 
-# Boucle principale
-while [ ! -f $TOOL_DIR/STOP ]; do 
-  
-  # attendre jusqu'à ce qu'il ne reste que 30 sec avant la fin du diaporama
-  # puis récupérer les 30 dernières photos (30 x 10s = 5min) (qui seront affichése a la prochaine itération)
-  NB_FIC=`find $DIR_SYMLINK -type l |wc -l`
-  DUREE_TEMPO=$(($NB_FIC * $DUREE_PHOTO - 30))
-  echo "[*] attendre $DUREE_TEMPO sec" &>>$LOG_FILE
-  sleep $DUREE_TEMPO &>/dev/null
-  echo "[+] fin attente" &>>$LOG_FILE
+}
+
+function make_links() {
   symlink_i=3
   find $PHOTO_DIR -maxdepth 1 -type f -printf "%C@ %p\n" | sort -n | cut -f2- -d" " | tail -$NB_PHOTO_DIAPO | tr '\n' '\0' | 
     while IFS= read -r -d '' file; do 
-        i=`printf "%02d" $symlink_i`
+        i=`printf "%03d" $symlink_i`
         #echo "link_$i" 
         ln -sf "$file" ${DIR_SYMLINK}/"link_$i"
         symlink_i=$(($symlink_i + 1))
     done
-  echo "[+] photos linkees dans le repertoire $DIR_SYMLINK" &>>$LOG_FILE
+}
 
-done
-
-rm $TOOL_DIR/diapo_mutex
+main
 
